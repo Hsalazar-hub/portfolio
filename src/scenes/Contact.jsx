07e3be3a26ea606d9a3465182c3ae0e92a8e43a5
Hsalazar-hub <56572487+Hsalazar-hub@users.noreply.github.com>
2022-12-31 19:29:03 -0400

    Update Contact.jsx

--- src/scenes/Contact.jsx
+++ src/scenes/Contact.jsx
@@ -71,7 +71,7 @@ const Contact = () => {
           <form
             target="_blank"
             onSubmit={onSubmit}
-            action="https://formsubmit.co/e8a5bdfa807605332f809e5656e27c6e"
+            action="https://formsubmit.co/f22780af2e002ddebbbf581e98091263"
             method="POST"
           >
             <input
