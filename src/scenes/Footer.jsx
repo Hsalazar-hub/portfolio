94a42c80dbc000b3a94deb40f75ee5653b23a71a
hdsalazar20 <hdsalazar20@gmail.com>
2023-02-27 12:54:58 -0400

    content fix

--- src/scenes/Footer.jsx
+++ src/scenes/Footer.jsx
@@ -1,21 +1,20 @@
 import SocialMediaIcons from "../components/SocialMediaIcons";
 
-const Footer = () =>{
-  return(
+const Footer = () => {
+  return (
     <footer className="h-64 bg-red pt-10">
       <div className="w-10/12 mx-auto">
-        <SocialMediaIcons/>
+        <SocialMediaIcons />
         <div className="md:flex justify-center md:justify-between text-center">
           <p className="font-playfair font-semibold text-2xl text-yellow">
             HECTOR SALAZAR
-            </p>
+          </p>
           <p className="font-playfair text-md text-yellow">
-            ©2022 DEVELOPER. All Rights Reserved
-            </p>
+            ©2023 DEVELOPER. All Rights Reserved
+          </p>
         </div>
       </div>
-
     </footer>
-  )
-}
+  );
+};
 export default Footer;
